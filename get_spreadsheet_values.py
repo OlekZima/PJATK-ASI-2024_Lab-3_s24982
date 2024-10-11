158c58566befd6b107b8287263e709852ee5488f
OlekZima <zimenkoalex34@gmail.com>
2024-10-11 20:13:34 +0200

    feat: add argument -f to get file

--- get_spreadsheet_values.py
+++ get_spreadsheet_values.py
@@ -42,28 +42,6 @@ class GoogleSheeter():
         pass
 
 
-# def main(cred, spreadsheet):
-#     credentials_info = cred
-#     if credentials_info is None:
-#         raise ValueError("GOOGLE_SHEETS_CREDENTIALS environment variable not set")
-#
-#     credentials_data = json.loads(credentials_info)
-#     credentials = service_account.Credentials.from_service_account_info(credentials_data, scopes=SCOPES)
-#
-#     try:
-#         service = build("sheets", "v4", credentials=credentials)
-#         sheet = service.spreadsheets()
-#
-#         result = sheet.values().get(spreadsheetId=spreadsheet, range="Sheet1!A1:C6").execute()
-#         values = result.get("values", [])
-#
-#         for row in values:
-#             print(row)
-#
-#     except HttpError as err:
-#         print(f"An error occurred: {err}")
-
-
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--cred", type=str, help="Google Credentials", required=True)
