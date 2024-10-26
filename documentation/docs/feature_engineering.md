e1a40394579537e0768a6ef9fc8e1731ecbca7ca
OlekZima <zimenkoalex34@gmail.com>
2024-10-26 22:13:35 +0200

    Add code for dealing with categorical features and appended changes to the docs

--- documentation/docs/feature_engineering.md
+++ documentation/docs/feature_engineering.md
@@ -1,2 +1,68 @@
 # Feature Engineering
 
+## Data encoding
+
+### Categorical values features
+
+Categorical features was coded as:
+
+!!! example
+
+    === "gender"
+
+        ``` markdown
+        Renamed to `gender_is_female`
+        `female`: True
+        `male`: False
+        ```
+
+    === "ethnicity"
+        
+        ``` markdown
+        OneHotEncoded
+        3 features were created:
+        `ethnicity_afam` `ethnicity_hispanic` and `ethnicity_other`
+        ```
+
+
+    === "fcollege/mcollege"
+
+        ``` markdown
+        Renamed to `is_fcollege`/`is_mcollege`
+        `yes`: True
+        `no`: False
+        ```
+    
+    === "home"
+        
+        ``` markdown
+        Renamed to `is_home`
+        `yes`: True
+        `no`: False
+        ```
+
+    === "urban"
+        
+        ``` markdown
+        Renamed to `is_urban`
+        `yes`: True
+        `no`: False
+        ```
+
+    === "income"
+        
+        ``` markdown
+        Renamed to `is_high_income`
+        `high`: True
+        `low`: False
+        ```
+
+    === "region"
+        
+        ``` markdown
+        Renamed to `is_region_west`
+        `west`: True
+        `other`: False
+        ```
+
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
 
 pd.set_option("display.max_columns", None)
 data_df = pd.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/AER/CollegeDistance.csv")
@@ -14,7 +15,6 @@ print(data_df.describe(include="all"))
 
 print(f"The dataset contains {data_df.shape[0]} rows and {data_df.shape[1]} columns.")
 
-
 numerical_cols = data_df.select_dtypes(include=["float64", "int64"]).columns
 print(numerical_cols)
 
@@ -25,7 +25,6 @@ print(categorical_cols)
 for col in categorical_cols:
     print(data_df[col].value_counts())
 
-
 for col in numerical_cols:
     plt.figure(figsize=(8, 6))
     sns.histplot(data_df[col], kde=True, bins=30)
@@ -34,16 +33,12 @@ for col in numerical_cols:
     plt.ylabel("Frequency")
     plt.savefig(f"documentation/docs/img/Distribution_of_{col}.png", bbox_inches="tight")
 
-
-
 for col in numerical_cols:
     plt.figure(figsize=(8, 6))
     sns.boxplot(x=data_df[col])
     plt.title(f"Box Plot of {col}")
     plt.xlabel(col)
     plt.savefig(f"documentation/docs/img/Boxplot_of_{col}.png", bbox_inches="tight")
-
-
 
 for col in categorical_cols:
     plt.figure(figsize=(8, 6))
@@ -53,8 +48,33 @@ for col in categorical_cols:
     plt.ylabel(col)
     plt.savefig(f"documentation/docs/img/Countplot_of_{col}.png", bbox_inches="tight")
 
+# ../.../././//././..././././.
 
-#
+data_df["gender"] = data_df["gender"].apply(lambda x: x == "female")
+data_df = data_df.rename(columns={"gender": "gender_is_female"})
+
+data_df["fcollege"] = data_df["fcollege"].apply(lambda x: x == "yes")
+data_df = data_df.rename(columns={"fcollege": "is_fcollege"})
+
+data_df["mcollege"] = data_df["mcollege"].apply(lambda x: x == "yes")
+data_df = data_df.rename(columns={"mcollege": "is_mcollege"})
+
+data_df["home"] = data_df["home"].apply(lambda x: x == "yes")
+data_df = data_df.rename(columns={"home": "is_home"})
+
+data_df["urban"] = data_df["urban"].apply(lambda x: x == "yes")
+data_df = data_df.rename(columns={"urban": "is_urban"})
+
+data_df["income"] = data_df["income"].apply(lambda x: x == "high")
+data_df = data_df.rename(columns={"income": "is_high_income"})
+
+data_df["region"] = data_df["region"].apply(lambda x: x == "west")
+data_df = data_df.rename(columns={"region": "is_region_west"})
+
+data_df = pd.get_dummies(data_df, columns=["ethnicity"], prefix=["ethnicity"], prefix_sep="_", dtype=int)
+
+print(data_df.head())
+print(data_df.info())
 
 plt.figure(figsize=(10, 8))
 corr = data_df.corr()
@@ -65,14 +85,12 @@ plt.show()
 sns.pairplot(data_df[numerical_cols])
 plt.show()
 
-
 for num_col in numerical_cols:
     for cat_col in categorical_cols:
         plt.figure(figsize=(8, 6))
         sns.boxplot(x=cat_col, y=num_col, data=data_df)
         plt.title(f"{num_col} by {cat_col}")
         plt.show()
-
 
 for num_col in numerical_cols:
     plt.figure(figsize=(8, 6))
